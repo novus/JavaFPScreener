0c1de6dbf869c1989514fc8d30486e7b4623e642
Chris Lewis <chris@thegodcode.net>
2013-01-09 17:53:53 -0500

    Update src/main/java/com/novus/fp/Functions.java

--- src/main/java/com/novus/fp/Functions.java
+++ src/main/java/com/novus/fp/Functions.java
@@ -11,20 +11,12 @@ public final class Functions {
 	 * A function for incrementing an integer value by 5.
 	 * ex: increment5I(5) // returns 10
 	 */
-	public static final F1<Integer, Integer> increment5I = new BaseF1<Integer,Integer>() {
-		public Integer apply(Integer i) {
-			return i + 5;
-		}
-	};
+	public static final F1<Integer, Integer> increment5I = null; // TODO implement!
 	
 	/** 
 	 * The integer squaring function.
 	 * ex: squareI(5) // returns 25
 	 */
-	public static final F1<Integer, Integer> squareI = new BaseF1<Integer,Integer>() {
-		public Integer apply(Integer i) {
-			return i * i;
-		}
-	};
+	public static final F1<Integer, Integer> squareI = null; // TODO implement!
 	
 }
